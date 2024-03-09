510a806f094009413cb70d5074281f9ec762e5c3
Gabriel Trindade Dias <95586355+yGabrielT@users.noreply.github.com>
2024-03-09 17:21:42 -0300

    Added oxygen system

---
+++ Assets/Scripts/UI/ChangeOxygenUi.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeOxygenUi : MonoBehaviour
+{
+    public TextMeshProUGUI OxygenTextUI;
+    public Image OxygenGraphicUI;
+    public RectTransform OxygenGraphicHolderUI;
+    public float oxygenTimerDivider = 2f;
+    private float oxygenAmount;
+    private bool losingOxygen;
+    private bool hasLostHalf;
+
+    void Start()
+    {
+
+        losingOxygen = true;
+        oxygenAmount = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(losingOxygen && oxygenAmount >= 0)
+        {
+            oxygenAmount -= Time.deltaTime / oxygenTimerDivider;
+            OxygenGraphicUI.fillAmount = ChangeStaminaUI.remap(oxygenAmount,0,100,0,1);
+        }
+        if(oxygenAmount <= 50f && !hasLostHalf)
+        {
+            hasLostHalf = true;
+            OxygenTextUI.DOColor(Color.red, .5f).SetEase(Ease.InOutCirc).OnComplete(() => OxygenTextUI.DOColor(Color.blue, .5f)).SetEase(Ease.InOutCirc).SetLoops(-1);
+            OxygenGraphicUI.DOColor(Color.red, .5f).SetEase(Ease.InOutCirc);
+            OxygenGraphicHolderUI.DOAnchorPos(new Vector2(32.2001f, 343f), 1f).SetEase(Ease.OutBack);
+        }
+    }
+}
--- Assets/Scripts/UI/ChangeStaminaUI.cs
+++ Assets/Scripts/UI/ChangeStaminaUI.cs
@@ -75,7 +75,7 @@ public class ChangeStaminaUI : MonoBehaviour
     }
 
 
-    private float remap(float val, float in1, float in2, float out1, float out2)
+    public static float remap(float val, float in1, float in2, float out1, float out2)
     {
         return out1 + (val - in1) * (out2 - out1) / (in2 - in1);
     }
